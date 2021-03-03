ecb450e963bad39c4e394bbdd82727b9ba3c88e1
BerkkanB <butun18@itu.edu.tr>
2021-03-03 14:09:27 +0300

    Update

--- assets/Screen/home.js
+++ assets/Screen/home.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { SafeAreaView, Text, View, StatusBar, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, View, StatusBar, StyleSheet, Pressable } from 'react-native';
 import Header from "../Component/header"
 import Event from "../Component/event"
 function home() {
     return (
        <SafeAreaView style={styles.conteiner}>
            <Header/>
-            
+            <View style={styles.chooseConteiner}>
+                <Pressable>
+                <Text style={{fontSize:22,fontWeight:"600"}}>Followings</Text>
+                </Pressable>
+                <Pressable>
+                <Text style={{marginLeft:10,fontSize:18,opacity:0.5}}>Public</Text>
+                </Pressable>
+            </View>
             <View style={styles.eventConteiner}>
                 <Event/>
             </View>
@@ -25,6 +32,14 @@ const styles = StyleSheet.create({
     eventConteiner:{
         width:"100%",
         alignItems: 'center', 
-        paddingTop:100
-    }
+        paddingTop:50
+    },
+    chooseConteiner:{
+        height:60,
+        width:"100%",
+        flexDirection:"row",
+        alignItems:"flex-end",
+        paddingHorizontal:20
+    },
+    
 });
