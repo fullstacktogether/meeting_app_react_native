b7cbdeeec29a1ead9c76b9f1d08d020f507ca69c
BerkkanB <butun18@itu.edu.tr>
2021-03-08 01:41:01 +0300

    UPDATE

--- assets/Screen/registerDetail.js
+++ assets/Screen/registerDetail.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { Text, View, StyleSheet, StatusBar, Pressable,Image,TextInput } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
+import * as ImageManipulator from 'expo-image-manipulator';
 
 
 function registerDetail(props) {
@@ -16,19 +17,50 @@ function registerDetail(props) {
         let pickerResult = await ImagePicker.launchImageLibraryAsync({allowsEditing:true,aspect:[1,1]});
         if (pickerResult.cancelled === true) {
             return;
-          }
-        setImage({ localUri: pickerResult.uri });
-      }
+        }
+
+        const manipulated = await ImageManipulator.manipulateAsync(
+            pickerResult.uri,
+            [{resize:{height:400,width:400}}],
+            { compress: 1, format: ImageManipulator.SaveFormat.PNG }
+        )
+        setImage(manipulated)
+
+         
+    }
+    const uploadAvatar = async () => {
+        const photo = {
+            uri: image.uri,
+            type: "image/jpg",
+            name: "photo.jpg",
+        };
+        const form = new FormData()
+        form.append("avatar", photo)
+        
+        fetch("http://192.168.1.104:3000/api/auth/me",{ method: 'PATCH',
+        headers:{  
+        "Content-Type": "multipart/form-data",
+        "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjA0NGRjZDFjMGMwYzEyMDA4MDE0YzgzIiwiaWF0IjoxNjE1MTI1NzEzLCJleHAiOjE2NDY2NjE3MTN9.QCy_bvqZ3ZZObtqm2IATiUBAHM_1eHhvnCjqUVgAQ40"
+        }, 
+        body : form
+        })
+            .then((res) => res.json())
+            .then((res) => { console.log("response" +JSON.stringify(res)); 
+                if(res.avatar_url){
+                    props.navigation.replace("HomeTabStack")
+                }
+            })
+            .catch((e) => console.log(e))
+            .done()
+    }
+
+
     return (
         <View style={styles.conteiner} >
-            <View>
-                <Text>Full Name</Text>
-                <TextInput/>
-            </View>
             <Pressable onPress={openImagePickerAsync}>
-                {image ? <Image style={styles.addImage} source={{ uri: image.localUri }} /> : <Image style={styles.addImage} source={require("../icon.png")} />}
+                {image ? <Image style={styles.addImage} source={{ uri: image.uri }} /> : <Image style={styles.addImage} source={require("../icon.png")} />}
             </Pressable>
-            <Pressable onPress={()=>props.navigation.replace("HomeTabStack")} >
+            <Pressable onPress={uploadAvatar} >
                 <Text>Confirm</Text>
             </Pressable>
             <StatusBar barStyle="light-content" />
